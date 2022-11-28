18c192bf2113e74d01c825208caa5a507ad496dc
claralv <lagunas.informatica@gmail.com>
2022-11-28 13:39:39 +0100

    Lista de la compra v1

--- app/src/main/java/com/example/listacompra/MainActivity.kt
+++ app/src/main/java/com/example/listacompra/MainActivity.kt
@@ -8,8 +8,6 @@ import android.view.inputmethod.InputMethodManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.listacompra.adapter.ListaAdapter
-import com.example.listacompra.database.Listacompra
-import com.example.listacompra.database.MiListaApp
 import com.example.listacompra.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -27,29 +25,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         elementos = ArrayList()
 
         getElementos()
 
     }
 
-
+    //Al pulsar sobre el cuadro de texto, se limpia
     fun clearFocus(){
         binding.texto.setText("")
     }
 
+    //Oculta el teclado cuando terminamos de escribir en el cuadro de texto
     fun Context.hideKeyboard() {
         val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(binding.texto.windowToken, 0)
     }
 
+    //Obtenemos todas las filas de la tabla y se las pasamos al adapter del recyclerView, para que las muestre
     fun getElementos() {
         CoroutineScope(Dispatchers.IO).launch  {
 
             runOnUiThread  {
                 adapter = ListaAdapter(elementos)
                 recyclerView= binding.recycler
-                recyclerView.setHasFixedSize(true)
                 recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
                 recyclerView.adapter = adapter
 
